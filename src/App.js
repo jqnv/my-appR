daf1dfa99c90525092a44fb079c169a3b5485f0b
Joaquin Vasquez <joaquin.vasquez@live.com>
2021-04-06 08:57:13 -0600

    Begin tasks app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{Component} from 'react';
+import tasks from "./sample/task"
+import Tasks from "./components/Tasks";
+/*class Helloworld extends React.Component{
+
+  state={
+    show:true
+  }
+
+  toggleShow=()=>{
+      this.setState({show:!this.state.show})
+  }
+  render(){
+    if(this.state.show){
+      return(<div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.myText}
+        <button onClick={this.toggleShow}>Toggle Show</button>
+      </div>)}
+    else{
+      return (
+          <h1>There is no elements
+          <button onClick={this.toggleShow}>Toggle Show</button></h1>
+      )
+    }
+  }
+}*/
+
+
+/*function Helloworld(props){
+  console.log(props)
+  return(
+      <div id="hello">
+        <h3>{props.subtitle}</h3>
+        {props.myText}
+      </div>
+  )
+}*/
+
+//const App=()=> <div>This is my component: <Helloworld/></div>
+/*
+class App extends React.Component{
+  render(){
+    <div>This is my component: <Helloworld/></div>
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+{    <div>
+      This is my component:
+      <Helloworld myText={"Hello Fast"} subtitle={"Lorem Ip"}/>
+      <Helloworld myText={"Hola Mundo"} subtitle={"Component Two"}/>
+      <Helloworld myText={"Hello"} subtitle={"Component 3"}/>
+    </div>}
   );
+}*/
+
+class App extends Component {
+  state={
+    tasks:tasks
+  }
+  render() {
+    return(
+    <div>
+      <Tasks tasks={this.state.tasks}/>
+
+    </div>)
+  }
 }
 
 export default App;
